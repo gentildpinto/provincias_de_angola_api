05beecfdcce281d7da18b216ea6bda1b090f743a
GentilPinto <gentil.junior.gp@gmail.com>
2020-06-06 13:46:40 +0100

    fix: template loader error

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'api/api#index'
-  namespace :api do
-    namespace :v1 do
+  root 'api/api#index', defaults: {format: :json}
+  namespace :api, defaults: {format: :json} do
+    namespace :v1, defaults: {format: :json} do
       resources :provincias, only: [:index, :show]
     end
   end
